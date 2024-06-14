26c2c1fbb6f636c7c4739087cdd38d3e466f8c1b
mingchi <mingchichen1213@gmail.com>
2024-06-14 15:45:02 +0800

    Freature: add include path

---
+++ Turret/Elevator.cpp
@@ -0,0 +1,10 @@
+#include <allegro5/base.h>
+#include <cmath>
+#include <string>
+
+#include "Engine/AudioHelper.hpp"
+#include "Bullet/FireBullet.hpp"
+#include "Engine/Group.hpp"
+#include "Scene/PlayScene.hpp"
+#include "Engine/Point.hpp"
+#include "Elevator.hpp"
